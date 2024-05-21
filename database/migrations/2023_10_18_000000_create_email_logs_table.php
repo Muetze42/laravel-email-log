9ca6b8255931cfb02aa80d5f02190c59d5c80806
Norman Huth <norman@huth.it>
2024-05-21 13:01:29 +0200

    refactor: nullable columns and use laravel 11 casts

--- database/migrations/2023_10_18_000000_create_email_logs_table.php
+++ database/migrations/2023_10_18_000000_create_email_logs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+return new class() extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,15 @@ return new class extends Migration
     {
         Schema::create('email_logs', function (Blueprint $table) {
             $table->id();
-            $table->string('subject');
-            $table->text('body');
-            $table->json('from');
-            $table->json('to');
-            $table->json('bbc');
-            $table->json('cc');
-            $table->json('reply_to');
-            $table->json('headers');
-            $table->json('attachments');
+            $table->string('subject')->nullable();
+            $table->text('body')->nullable();
+            $table->json('from')->nullable();
+            $table->json('to')->nullable();
+            $table->json('bbc')->nullable();
+            $table->json('cc')->nullable();
+            $table->json('reply_to')->nullable();
+            $table->json('headers')->nullable();
+            $table->json('attachments')->nullable();
             $table->boolean('is_html');
             $table->tinyInteger('priority', false, true);
             $table->nullableMorphs('authenticatable');
